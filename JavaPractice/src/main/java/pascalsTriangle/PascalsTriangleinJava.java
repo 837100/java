d4de8f204895387317b1ca0c846f84ad55878eb1
huon <huon@192.168.11.4>
2024-05-17 22:36:50 +0900

    create project

---
+++ JavaPractice/src/main/java/pascalsTriangle/PascalsTriangleinJava.java
@@ -0,0 +1,10 @@
+package pascalsTriangle;
+
+public class PascalsTriangleinJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int input=5; // 원하는 수 만큼 변경 가능
+	}
+
+}
